2d18157ee9124f5fc3aeaf59cf789bab307ab579
Donald Whitely <dswhitely1@gmail.com>
2019-10-24 21:06:50 -0400

    Commit 1

--- src/store/weather/weatherReducer.js
+++ src/store/weather/weatherReducer.js
@@ -3,17 +3,17 @@ import { FETCH_WEATHER_START, FETCH_WEATHER_SUCCESS, FETCH_WEATHER_FAILURE } fro
 const initialState = {
     isLoading: false,
     errors: null,
-    weatherAlerts: []
+    alerts: []
 }
 
 export function reducer(state=initialState, action) {
     switch (action.type) {
         case FETCH_WEATHER_START:
-            return {...state, isLoading: true, errors: null, weatherAlerts:[]}
+            return {...state, isLoading: true, errors: null, alerts:[]}
             case FETCH_WEATHER_SUCCESS:
-                return {...state, isLoading: false, errors: null, weatherAlerts: action.payload}
+                return {...state, isLoading: false, errors: null, alerts: action.payload}
                 case FETCH_WEATHER_FAILURE:
-                    return {...state, isLoading: false, errors: action.payload, weatherAlerts:[]}
+                    return {...state, isLoading: false, errors: action.payload, alerts:[]}
         default:
             return state;
     }
