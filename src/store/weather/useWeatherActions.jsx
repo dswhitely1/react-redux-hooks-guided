516b5349d6db6becc0555774940e33f55d44814b
Donald Whitely <dswhitely1@gmail.com>
2019-10-24 21:06:56 -0400

    Commit 2

--- src/store/weather/useWeatherActions.jsx
+++ src/store/weather/useWeatherActions.jsx
@@ -1,15 +1,15 @@
 import {useCallback} from 'react';
 import {useDispatch} from 'react-redux'
 import axios from 'axios';
-import { FETCH_WEATHER_START, FETCH_WEATHER_FAILURE } from './weatherTypes';
+import { FETCH_WEATHER_START, FETCH_WEATHER_FAILURE, FETCH_WEATHER_SUCCESS } from './weatherTypes';
 
 export const useWeatherActions = () => {
     const dispatch=useDispatch();
 
     const fetchWeatherAlerts = useCallback(()=> {
         dispatch({type: FETCH_WEATHER_START});
-        axios.get('').then(res => {
-            console.log(res.data)
+        axios.get('https://api.weather.gov/alerts').then(res => {
+            dispatch({type: FETCH_WEATHER_SUCCESS, payload: res.data.features})
         }).catch(err => dispatch({type: FETCH_WEATHER_FAILURE, payload: err.response}))
     }, [dispatch])
 
